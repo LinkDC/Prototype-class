5bf5398f8464102fac32a662f420eff94a67f777
linkdcz <tim.vikulov@gmail.com>
2022-04-11 19:07:56 +0300

    Добавьте файлы проекта.

---
+++ Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public int Fuel { get; set; }
+
+        public List<int> Amount { get; set; }
+
+        public Car()
+        {
+            Name = "Мошина";
+            Fuel = 100;
+            Amount = new List<int>();
+        }
+        public Car(string _name, int _fuel)
+        {
+            Name = _name;
+            Fuel = _fuel;
+            Amount = new List<int>();
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Fuel);
+        }
+        public void Move(int rass)
+        {
+            Fuel -= rass;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace method // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Car mers = new Car();
+            Car gaz = new Car("Газель", 300);
+            gaz.PrintInfo();
+            gaz.Move(50);
+            gaz.PrintInfo();
+        }
+
+    }
+}
